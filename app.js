631cee5258bb8f530b4761b60e440012e1cfeaaf
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-04-12 23:01:40 -0500

    Corrected issues but same-site cookies

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 function generateCat(){
 
 fetch("https://api.thecatapi.com/v1/images/search")
-    .then((response) => response.json())
+    .then((response) => response)
     .then(data => {
+        if(typeof data == "object")
+            console.log("Wuju!")
         var image = document.createElement("img");
         image.setAttribute("id","bullshit")
         var div = document.getElementById("flex-cat-gen");
-        image.src = data["url"];
+        var url = data["url"];
+        console.log(url)
+        image.src = url;
         div.appendChild(image);
     })
     .catch(error => {
