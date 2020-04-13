ad3effccbe0ae7e68607a583b1402781686af5f5
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-04-12 21:17:40 -0500

    Deleted unused functions

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-function ageInDays(){
-    var birthYear = prompt("Birth year?");
-    var ageInDayss = (2020 - birthYear) * 365;
-    var h1 = document.createElement("h1");
-    var textAnswer = document.createTextNode("You are " + ageInDayss + " years old!");
-    h1.setAttribute("id","ageInDays");
-    h1.appendChild(textAnswer);
-    document.getElementById("flex-box-result").appendChild(h1);
-}
-
-function reset(){
-    document.getElementById("ageInDays").remove();
-}   
-
 function generateCat(){
     var url = "cat_images/cat";
     var nCat = Math.round((Math.random()*100000)%25);
